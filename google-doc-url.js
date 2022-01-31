6ccb955a955a280f30335930e245be193e29ee4c
control-g-lab <85198072+control-g-lab@users.noreply.github.com>
2022-01-31 15:17:03 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1aFdBJBrWYRK5FckM4NyZh4KPpzTvXkQvpEsoFv1mwsk/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AbK3frWIRvHAj-g5Wm-2E0q7';
+var googleApiKey = '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';
